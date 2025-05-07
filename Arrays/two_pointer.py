b7814ed89036e36fa79a5c04ad172a0bbb71cf16
Gabriel <g.silva01@icloud.com>
2025-05-07 19:43:55 -0300

    two_pointer

---
+++ Arrays/two_pointer.py
@@ -0,0 +1,16 @@
+def reverse_words(s):
+    res = ''
+    l, r = 0, 0
+
+    while r < len(s):
+        if s[r] != ' ':
+            r += 1
+        else:
+            res += s[l:r+1][::-1]
+            r += 1
+            l = r
+
+    res += ' '
+    res += s[l:r+1][::-1]
+
+    return res[1:]
